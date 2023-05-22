47efd7cb37e3a136a82bd02143e80ec70259831b
Iskander Kudashev <kudashev.iskander13@gmail.com>
2023-05-23 00:12:29 +0300

    chore: read file from client

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+import os
+from io import BytesIO
+
+from fastapi import FastAPI, File, UploadFile
+import pandas as pd
 
 app = FastAPI()
 
@@ -11,3 +15,25 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+files = {}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    # transform the XLSX file to a Pandas DataFrame
+    df = pd.read_excel(BytesIO(contents))
+
+    # создаем директорию "files", если ее нет
+    if not os.path.exists('files'):
+        os.makedirs('files')
+
+    # сохраняем файл в директорию "files" с помощью Pandas
+    file_name = os.path.join('files', file.filename)
+    df.to_excel(file_name, index=False)
+
+    print(df)
+
+    return {"filename": file.filename, "message": "File uploaded successfully"}
