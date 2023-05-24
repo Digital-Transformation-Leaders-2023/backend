1d3c869ac2bc4613afe3e295fa28a8c512a41bee
nikolaykochubeev <nikolay.kochubeev@gmail.com>
2023-05-24 22:10:01 +0300

    Fix

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
-import os
-from io import BytesIO
-
-from fastapi import FastAPI, File, UploadFile
 import pandas as pd
-from pymongo import MongoClient
-
 import os
-from dotenv import load_dotenv
 
-from .database import Session, engine
-from . import models
+from app.models import User
+from fastapi import FastAPI, File, UploadFile, Depends
+from pymongo import MongoClient
+from io import BytesIO
+from dotenv import load_dotenv
+from .database import Session
 
 load_dotenv()
 
@@ -25,11 +22,12 @@ mongo_uri = f"mongodb://{mongo_user}:{mongo_password}@{mongo_host}:{mongo_port}/
 
 client = MongoClient(mongo_uri)
 
-os.environ['DATABASE_URL'] = 'postgresql://user:password@db:5432/mydatabase'
+os.environ['DATABASE_URL'] = os.environ.get("DATABASE_URL", "postgresql://user:password@localhost:5432/mydatabase")
+
 
 def get_db():
+    db = Session()
     try:
-        db = Session()
         yield db
     finally:
         db.close()
@@ -72,6 +70,15 @@ async def create_upload_file(file: UploadFile = File(...)):
     print(df)
 
     return {"filename": file.filename, "message": "File uploaded successfully"}
+
+
+@app.post("/users/")
+def create_user(db: Session = Depends(get_db)):
+    db_user = User(email="fuck", name="python")
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user.id
 
 
 @app.on_event("shutdown")
