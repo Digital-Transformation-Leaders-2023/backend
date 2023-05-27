e8732f52d8229cda46e40e5fc1f47261c98b8c98
Iskander Kudashev <kudashev.iskander13@gmail.com>
2023-05-27 23:30:52 +0300

    fix: filter ages

--- app/internal/model/report_filter.py
+++ app/internal/model/report_filter.py
@@ -3,15 +3,10 @@ from typing import Optional
 
 
 @dataclass(frozen=True)
-class AgesFilter:
-    ages: [str]
-
-
-@dataclass(frozen=True)
 class ReportFilter:
     limit: int = 10
     skip: int = 0
     is_favorite: Optional[bool] = None
     age: Optional[str] = None
-    sex: Optional[AgesFilter] = None
+    sex: Optional[list[str]] = None
     mkb_code: Optional[str] = None
