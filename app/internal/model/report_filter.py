be5dcb6b10031fb3cae3d03c7f544a5be48d862e
Iskander Kudashev <kudashev.iskander13@gmail.com>
2023-05-27 23:27:29 +0300

    feat: ages like list of strings

--- app/internal/model/report_filter.py
+++ app/internal/model/report_filter.py
@@ -3,10 +3,15 @@ from typing import Optional
 
 
 @dataclass(frozen=True)
+class AgesFilter:
+    ages: [str]
+
+
+@dataclass(frozen=True)
 class ReportFilter:
     limit: int = 10
     skip: int = 0
     is_favorite: Optional[bool] = None
     age: Optional[str] = None
-    sex: Optional[str] = None
+    sex: Optional[AgesFilter] = None
     mkb_code: Optional[str] = None
